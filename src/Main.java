58c38c3d8eb36a24666ad9eb9d8d38abbf8adecb
Qsenia <oksana.kholnova@gmail.com>
2023-02-20 14:05:37 +0300

    Adding branch Stat

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = new int[]{1000, 50, 200, 40, 60, 70, 90, 570, 1000, 40};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println("Максимальные продажи в литрах: ");
-        System.out.println(salesManager.max() + " л");
+        System.out.println("Максимальные продажи в литрах: "+ salesManager.max() + " л");
+        System.out.println("Минимальные продажи в литрах: "+salesManager.min() + " л");
+        System.out.println("Обрезанные средние продажи в л: "+salesManager.average()+ " л");
     }
 }
