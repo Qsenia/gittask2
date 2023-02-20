d836e256c6b10fb2e7280a5cc30b5ea70442966e
Qsenia <oksana.kholnova@gmail.com>
2023-02-20 19:11:56 +0300

    Merging longer

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,18 +14,18 @@ public class SalesManager {
         }
         return max;
     }
-    public int min() {
-        int min = Integer.MAX_VALUE;
-        for (int sale : sales) {
+    public long min() {
+        long min = Integer.MAX_VALUE;
+        for (long sale : sales) {
             if (sale < min) {
                 min = sale;
             }
         }
         return min;
     }
-    public int average() {
+    public long average() {
         int sum = 0;
-        for (int sale : sales) {
+        for (long sale : sales) {
             sum += sale;
         }
         return (sum-(min()+max()))/ (sales.length-2);
